5792ffd5884fca18725b511d17a02afa778e903d
unknown <LEBRUN_G@PF2801-W7.cofiroute.com>
2013-10-21 18:42:39 +0200

    ajout spring security + login

---
+++ backend-map/src/main/java/com.oxiane.caveavin/security/AjaxAuthenticationSuccessHandler.java
@@ -0,0 +1,35 @@
+/* +-----------------------------------------------------------------------------------------------+
+ *                                                                                                
+ *  Titre          : AjaxAuthenticationSuccessHandler                                                                 
+ *  Description    : Définition                                         
+ *  Copyright      : Cofiroute 2013                                                            
+ *  Societe        : Cofiroute                                                                    
+ *  Author         : LEBRUN_G                                                                      
+ *  Version        : 1.0.0                                                                        
+ *  Package        : com.oxiane.caveavin.security                                                              
+ *  Parametre VM   : Neant                                                                        
+ *  Parametre main : Neant                                                                        
+ *                                                                                                
+ * +-----------------------------------------------------------------------------------------------+
+ *  Historique des mises a jour :                                                                 
+ *  Auteur Date       Description                                                                 
+ *  ------- ------------------------------------------------------------------------------------- 
+ *  LEBRUN_G 21/10/13 Creation                                                                      
+ *                                                                                                
+ * +-----------------------------------------------------------------------------------------------+
+ *  Remarques :                                                                                   
+ *                                                                                                
+ * +-----------------------------------------------------------------------------------------------+
+ *  RAF : Neant                                                                                   
+ *                                                                                                
+ * +-----------------------------------------------------------------------------------------------+
+ */
+package com.oxiane.caveavin.security;
+
+/**
+ * User: LEBRUN_G
+ * Date: 21/10/13
+ * Time: 11:16
+ */
+public class AjaxAuthenticationSuccessHandler {
+}
